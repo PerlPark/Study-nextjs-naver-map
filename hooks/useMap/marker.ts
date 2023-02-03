dca08a403140c1047aafd3379db1abe67e7a3aed
perlpark <hello@jinjoo.me>
2023-02-03 13:57:35 +0900

    chore: marker.ts 파일 삭제

--- hooks/useMap/marker.ts
+++
@@ -1,4 +0,0 @@
-const marker =
-  '<div style="width:12px;height:12px;background-color:#0267FF;border:1px #ffffff solid;border-radius:6px;box-shadow: 0px 0px 7px 1px #0267FF;" />';
-
-export default marker;
